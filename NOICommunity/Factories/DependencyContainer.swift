d8405eb109dd1185467ebedfc347c05233a6ca18
Matteo Matassoni <4108197+matax87@users.noreply.github.com>
2025-11-19 10:52:34 +0100

    Make auth state invalidation explicit instead of implicit during validation

--- NOICommunity/Factories/DependencyContainer.swift
+++ NOICommunity/Factories/DependencyContainer.swift
@@ -26,97 +26,97 @@ import VimeoVideoThumbnailClient
 // MARK: - DependencyContainer
 
 final class DependencyContainer {
-    
-    let appPreferencesClient: AppPreferencesClient
-    let isAutorizedClient: IsAutorizedClient
-	let oidcAuthValidator: OIDCAuthValidator
-    let authClient: AuthClient
-    let eventShortClient: EventShortClient
-    let eventShortTypesClient: EventShortTypesClient
-    let artileClient: ArticlesClient
-    let articleTagsClient: ArticleTagsClient
-    let peopleClient: PeopleClient
+
+	let appPreferencesClient: AppPreferencesClient
+	let isAutorizedClient: IsAutorizedClient
+	let oidcAuthStateValidator: OIDCAuthStateValidator
+	let authClient: AuthClient
+	let eventShortClient: EventShortClient
+	let eventShortTypesClient: EventShortTypesClient
+	let artileClient: ArticlesClient
+	let articleTagsClient: ArticleTagsClient
+	let peopleClient: PeopleClient
 	let vimeoVideoThumbnailClient: VimeoVideoThumbnailClient
 
-    private var _userInfoCache: Cache<CacheKey, UserInfo>?
-    private var userInfoCache: Cache<CacheKey, UserInfo>! {
-        get {
-            if let userInfoCache = _userInfoCache {
-                return userInfoCache
-            } else {
-                let userInfoCache = Cache<CacheKey, UserInfo>()
-                _userInfoCache = userInfoCache
-                return userInfoCache
-            }
-        }
-        set {
-            _userInfoCache = newValue
-        }
-    }
-    
-    private var subscriptions: Set<AnyCancellable> = []
+	private var _userInfoCache: Cache<CacheKey, UserInfo>?
+	private var userInfoCache: Cache<CacheKey, UserInfo>! {
+		get {
+			if let userInfoCache = _userInfoCache {
+				return userInfoCache
+			} else {
+				let userInfoCache = Cache<CacheKey, UserInfo>()
+				_userInfoCache = userInfoCache
+				return userInfoCache
+			}
+		}
+		set {
+			_userInfoCache = newValue
+		}
+	}
 
-    init(
-        appPreferencesClient: AppPreferencesClient,
-        isAutorizedClient: @escaping IsAutorizedClient,
-		oidcAuthValidator: OIDCAuthValidator,
-        authClient: AuthClient,
-        eventShortClient: EventShortClient,
-        eventShortTypesClient: EventShortTypesClient,
-        articleClient: ArticlesClient,
-        articleTagsClient: ArticleTagsClient,
-        peopleClient: PeopleClient,
+	private var subscriptions: Set<AnyCancellable> = []
+
+	init(
+		appPreferencesClient: AppPreferencesClient,
+		isAutorizedClient: @escaping IsAutorizedClient,
+		oidcAuthStateValidator: OIDCAuthStateValidator,
+		authClient: AuthClient,
+		eventShortClient: EventShortClient,
+		eventShortTypesClient: EventShortTypesClient,
+		articleClient: ArticlesClient,
+		articleTagsClient: ArticleTagsClient,
+		peopleClient: PeopleClient,
 		vimeoVideoThumbnailClient: VimeoVideoThumbnailClient
-    ) {
-        self.appPreferencesClient = appPreferencesClient
-        self.isAutorizedClient = isAutorizedClient
-		self.oidcAuthValidator = oidcAuthValidator
-        self.authClient = authClient
-        self.eventShortClient = eventShortClient
-        self.eventShortTypesClient = eventShortTypesClient
-        self.artileClient = articleClient
-        self.articleTagsClient = articleTagsClient
+	) {
+		self.appPreferencesClient = appPreferencesClient
+		self.isAutorizedClient = isAutorizedClient
+		self.oidcAuthStateValidator = oidcAuthStateValidator
+		self.authClient = authClient
+		self.eventShortClient = eventShortClient
+		self.eventShortTypesClient = eventShortTypesClient
+		self.artileClient = articleClient
+		self.articleTagsClient = articleTagsClient
 		self.peopleClient = peopleClient
 		self.vimeoVideoThumbnailClient = vimeoVideoThumbnailClient
 
-        NotificationCenter
-            .default
-            .publisher(for: logoutNotification)
-            .sink { [weak self] _ in
-                self?.userInfoCache = nil
-            }
-            .store(in: &subscriptions)
-    }
-    
+		NotificationCenter
+			.default
+			.publisher(for: logoutNotification)
+			.sink { [weak self] _ in
+				self?.userInfoCache = nil
+			}
+			.store(in: &subscriptions)
+	}
+
 }
 // MARK: ClientFactory
 
 extension DependencyContainer: ClientFactory {
-    
-    func makeIsAutorizedClient() -> IsAutorizedClient {
-        isAutorizedClient
-    }
-    
-    
-    func makeAppPreferencesClient() -> AppPreferencesClient {
-        appPreferencesClient
-    }
 
-	func makeOIDCAuthValidator() -> any OIDCAuthValidator {
-		oidcAuthValidator
+	func makeIsAutorizedClient() -> IsAutorizedClient {
+		isAutorizedClient
 	}
 
-    func makeAuthClient() -> AuthClient {
-        authClient
-    }
-    
-    func makeArticlesClient() -> ArticlesClient {
-        artileClient
-    }
-    
-    func makePeopleClient() -> PeopleClient {
-        peopleClient
-    }
+
+	func makeAppPreferencesClient() -> AppPreferencesClient {
+		appPreferencesClient
+	}
+
+	func makeOIDCAuthValidator() -> any OIDCAuthStateValidator {
+		oidcAuthStateValidator
+	}
+
+	func makeAuthClient() -> AuthClient {
+		authClient
+	}
+
+	func makeArticlesClient() -> ArticlesClient {
+		artileClient
+	}
+
+	func makePeopleClient() -> PeopleClient {
+		peopleClient
+	}
 
 	func makeVimeoVideoThumbnailClient() -> any VimeoVideoThumbnailClient {
 		vimeoVideoThumbnailClient
@@ -127,20 +127,20 @@ extension DependencyContainer: ClientFactory {
 // MARK: ViewModelFactory
 
 extension DependencyContainer: ViewModelFactory {
-    
-    func makeEventsViewModel(
-        showFiltersHandler: @escaping () -> Void
-    ) -> EventsViewModel {
-        let supportedPreferredLanguage = Bundle.main.preferredLocalizations
-            .lazy
-            .compactMap(Language.init(rawValue:))
-            .first
-        return .init(
-            eventShortClient: eventShortClient,
-            language: supportedPreferredLanguage,
-            showFiltersHandler: showFiltersHandler
-        )
-    }
+
+	func makeEventsViewModel(
+		showFiltersHandler: @escaping () -> Void
+	) -> EventsViewModel {
+		let supportedPreferredLanguage = Bundle.main.preferredLocalizations
+			.lazy
+			.compactMap(Language.init(rawValue:))
+			.first
+		return .init(
+			eventShortClient: eventShortClient,
+			language: supportedPreferredLanguage,
+			showFiltersHandler: showFiltersHandler
+		)
+	}
 
 
 	func makeEventDetailsViewModel(eventId: String) -> EventDetailsViewModel {
@@ -151,60 +151,60 @@ extension DependencyContainer: ViewModelFactory {
 		.init(eventShortClient: eventShortClient, event: event)
 	}
 
-    func makeEventFiltersViewModel(
-        showFilteredResultsHandler: @escaping () -> Void
-    ) -> EventFiltersViewModel {
-        .init(
-            eventShortTypes: eventShortTypesClient,
-            showFilteredResultsHandler: showFilteredResultsHandler
-        )
-    }
-    
-    func makeWelcomeViewModel() -> WelcomeViewModel {
-        .init(with: [
-            .init(
-                backgroundImageName: "welcome_news",
-                title: .localized("onboarding_news_title"),
-                description: .localized("onboarding_news_text")
-            ),
-            .init(
-                backgroundImageName: "noisteria_außen",
-                title: .localized("onboarding_events_title"),
-                description: .localized("onboarding_events_text")
-            ),
-            .init(
-                backgroundImageName: "welcome_meet",
-                title: .localized("onboarding_meetup_title"),
-                description: .localized("onboarding_meetup_text")
-            )
-        ])
-    }
+	func makeEventFiltersViewModel(
+		showFilteredResultsHandler: @escaping () -> Void
+	) -> EventFiltersViewModel {
+		.init(
+			eventShortTypes: eventShortTypesClient,
+			showFilteredResultsHandler: showFilteredResultsHandler
+		)
+	}
 
-    func makeLoadUserInfoViewModel() -> LoadUserInfoViewModel {
-        .init(
-            authClient: makeAuthClient(),
-            peopleClient: makePeopleClient(),
-            appPreferencesClient: makeAppPreferencesClient(),
-            cache: userInfoCache
-        )
-    }
-    
-    func makeMyAccountViewModel() -> MyAccountViewModel {
-        .init(
-            authClient: makeAuthClient(),
-            appPreferencesClient: makeAppPreferencesClient(),
-            cache: userInfoCache
-        )
-    }
-    
-    func makeNewsListViewModel(
-        showFiltersHandler: @escaping () -> Void
-    ) -> NewsListViewModel {
-        .init(
-            articlesClient: makeArticlesClient(),
-            showFiltersHandler: showFiltersHandler
-        )
-    }
+	func makeWelcomeViewModel() -> WelcomeViewModel {
+		.init(with: [
+			.init(
+				backgroundImageName: "welcome_news",
+				title: .localized("onboarding_news_title"),
+				description: .localized("onboarding_news_text")
+			),
+			.init(
+				backgroundImageName: "noisteria_außen",
+				title: .localized("onboarding_events_title"),
+				description: .localized("onboarding_events_text")
+			),
+			.init(
+				backgroundImageName: "welcome_meet",
+				title: .localized("onboarding_meetup_title"),
+				description: .localized("onboarding_meetup_text")
+			)
+		])
+	}
+
+	func makeLoadUserInfoViewModel() -> LoadUserInfoViewModel {
+		.init(
+			authClient: makeAuthClient(),
+			peopleClient: makePeopleClient(),
+			appPreferencesClient: makeAppPreferencesClient(),
+			cache: userInfoCache
+		)
+	}
+
+	func makeMyAccountViewModel() -> MyAccountViewModel {
+		.init(
+			authClient: makeAuthClient(),
+			appPreferencesClient: makeAppPreferencesClient(),
+			cache: userInfoCache
+		)
+	}
+
+	func makeNewsListViewModel(
+		showFiltersHandler: @escaping () -> Void
+	) -> NewsListViewModel {
+		.init(
+			articlesClient: makeArticlesClient(),
+			showFiltersHandler: showFiltersHandler
+		)
+	}
 
 	func makeNewsDetailsViewModel(
 		newsId: String
@@ -216,30 +216,30 @@ extension DependencyContainer: ViewModelFactory {
 		news: Article
 	) -> NewsDetailsViewModel {
 		.init(articlesClient: makeArticlesClient(), news: news)
-    }
-    
-    func makeNewsFiltersViewModel(
-        showFilteredResultsHandler: @escaping () -> Void
-    ) -> NewsFiltersViewModel {
-        .init(
-            articleTagsClient: articleTagsClient,
+	}
+
+	func makeNewsFiltersViewModel(
+		showFilteredResultsHandler: @escaping () -> Void
+	) -> NewsFiltersViewModel {
+		.init(
+			articleTagsClient: articleTagsClient,
 			appPreferencesClient: makeAppPreferencesClient(),
 			articlesClient: makeArticlesClient(),
-            showFilteredResultsHandler: showFilteredResultsHandler
-        )
-    }
-    
-    func makePeopleViewModel() -> PeopleViewModel {
-        .init(authClient: makeAuthClient(), peopleClient: makePeopleClient())
-    }
+			showFilteredResultsHandler: showFilteredResultsHandler
+		)
+	}
 
-    func makeComeOnBoardOnboardingViewModel() -> ComeOnBoardOnboardingViewModel {
-        .init(appPreferencesClient: makeAppPreferencesClient())
-    }
+	func makePeopleViewModel() -> PeopleViewModel {
+		.init(authClient: makeAuthClient(), peopleClient: makePeopleClient())
+	}
 
-    func makeDeveloperToolsViewModel() -> DeveloperToolsViewModel {
-        .init()
-    }
+	func makeComeOnBoardOnboardingViewModel() -> ComeOnBoardOnboardingViewModel {
+		.init(appPreferencesClient: makeAppPreferencesClient())
+	}
+
+	func makeDeveloperToolsViewModel() -> DeveloperToolsViewModel {
+		.init()
+	}
 
 	func makeReauthenticatePopUpViewModel() -> ReauthenticatePopUpViewModel {
 		.init()
@@ -250,10 +250,10 @@ extension DependencyContainer: ViewModelFactory {
 // MARK: ViewControllerFactory
 
 extension DependencyContainer: ViewControllerFactory {
-    
-    func makeEventListViewController() -> EventListViewController {
-        .init(items: [])
-    }
+
+	func makeEventListViewController() -> EventListViewController {
+		.init(items: [])
+	}
 
 	func makeEventPageViewController(
 		viewModel: EventDetailsViewModel
@@ -261,76 +261,76 @@ extension DependencyContainer: ViewControllerFactory {
 		.init(viewModel: viewModel, dependencyContainer: self)
 	}
 
-    func makeEventFiltersViewController(
-        viewModel: EventFiltersViewModel
-    ) -> EventFiltersViewController {
-        .init(viewModel: viewModel)
-    }
-    
-    func makeNewsFiltersViewController(viewModel: NewsFiltersViewModel) -> NewsFiltersViewController {
-        .init(viewModel: viewModel)
-    }
-    
-    func makeWelcomeViewController(
-        viewModel: WelcomeViewModel
-    ) -> AuthWelcomeViewController {
-        .init(viewModel: viewModel)
-    }
+	func makeEventFiltersViewController(
+		viewModel: EventFiltersViewModel
+	) -> EventFiltersViewController {
+		.init(viewModel: viewModel)
+	}
 
-    func makeMyAccountViewController(
-        viewModel: MyAccountViewModel
-    ) -> MyAccountViewController {
-        .init(viewModel: viewModel)
-    }
-    
-    func makeAccessNotGrantedViewController(
-        viewModel: MyAccountViewModel
-    ) -> AccessNotGrantedViewController {
-        .init(viewModel: viewModel)
-    }
-    
-    func makeNewsViewController(
-        viewModel: NewsListViewModel
-    ) -> NewsViewController {
-        .init(viewModel: viewModel)
-    }
-    
+	func makeNewsFiltersViewController(viewModel: NewsFiltersViewModel) -> NewsFiltersViewController {
+		.init(viewModel: viewModel)
+	}
+
+	func makeWelcomeViewController(
+		viewModel: WelcomeViewModel
+	) -> AuthWelcomeViewController {
+		.init(viewModel: viewModel)
+	}
+
+	func makeMyAccountViewController(
+		viewModel: MyAccountViewModel
+	) -> MyAccountViewController {
+		.init(viewModel: viewModel)
+	}
+
+	func makeAccessNotGrantedViewController(
+		viewModel: MyAccountViewModel
+	) -> AccessNotGrantedViewController {
+		.init(viewModel: viewModel)
+	}
+
+	func makeNewsViewController(
+		viewModel: NewsListViewModel
+	) -> NewsViewController {
+		.init(viewModel: viewModel)
+	}
+
 	func makeNewsPageViewController(
 		viewModel: NewsDetailsViewModel
 	) -> NewsPageViewController {
 		.init(viewModel: viewModel, dependencyContainer: self)
-    }
-    
-    func makeMeetMainViewController(
-        viewModel: PeopleViewModel
-    ) -> MeetMainViewController {
-        .init(viewModel: viewModel)
-    }
-    
-    func makePersonDetailsViewController(
-        person: Person,
-        company: Company?
-    ) -> PersonDetailsViewController {
-        .init(person: person, company: company)
-    }
-    
-    func makeCompaniesFiltersViewController(
-        viewModel: PeopleViewModel
-    ) -> CompaniesFiltersViewController {
-        .init(peopleViewModel: viewModel)
-    }
+	}
 
-    func makeComeOnBoardOnboardingViewController(
-        viewModel: ComeOnBoardOnboardingViewModel
-    ) -> ComeOnBoardOnboardingViewController {
-        .init(viewModel: viewModel)
-    }
-    
-    func makeDeveloperToolsViewController(
-        viewModel: DeveloperToolsViewModel
-    ) -> DeveloperToolsViewController {
-        .init(viewModel: viewModel)
-    }
+	func makeMeetMainViewController(
+		viewModel: PeopleViewModel
+	) -> MeetMainViewController {
+		.init(viewModel: viewModel)
+	}
+
+	func makePersonDetailsViewController(
+		person: Person,
+		company: Company?
+	) -> PersonDetailsViewController {
+		.init(person: person, company: company)
+	}
+
+	func makeCompaniesFiltersViewController(
+		viewModel: PeopleViewModel
+	) -> CompaniesFiltersViewController {
+		.init(peopleViewModel: viewModel)
+	}
+
+	func makeComeOnBoardOnboardingViewController(
+		viewModel: ComeOnBoardOnboardingViewModel
+	) -> ComeOnBoardOnboardingViewController {
+		.init(viewModel: viewModel)
+	}
+
+	func makeDeveloperToolsViewController(
+		viewModel: DeveloperToolsViewModel
+	) -> DeveloperToolsViewController {
+		.init(viewModel: viewModel)
+	}
 
 	func makeReauthenticatePopUpViewController(
 		viewModel: ReauthenticatePopUpViewModel
--- NOICommunity/SceneDelegate.swift
+++ NOICommunity/SceneDelegate.swift
@@ -33,144 +33,144 @@ private let accessGroupKey = "5V2Q9SWB7H.it.bz.noi.community"
 // MARK: - SceneDelegate
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
-    var window: UIWindow?
-    
-    var currentAuthorizationFlow: OIDExternalUserAgentSession?
-    
-    lazy var eventsCache: Cache<EventShortTypesClient.CacheKey, [EventsFilter]> = Cache()
+	
+	var window: UIWindow?
+	
+	var currentAuthorizationFlow: OIDExternalUserAgentSession?
+	
+	lazy var eventsCache: Cache<EventShortTypesClient.CacheKey, [EventsFilter]> = Cache()
 	lazy var articleTagsCache = Cache<ArticleTagsClientCacheKey, ArticleTagListResponse>()
-
-    lazy var dependencyContainer: DependencyContainer = {
-        let tokenStorage = KeychainAuthStateStorageClient(
-            keyChainAccessGroup: accessGroupKey
-        )
-        return DependencyContainer(
-            appPreferencesClient: .live(),
-            isAutorizedClient: {
-                guard let authState = tokenStorage.state
-                else { return false }
-                
-                
-                return authState.isAuthorized
+	
+	lazy var dependencyContainer: DependencyContainer = {
+		let tokenStorage = KeychainAuthStateStorageClient(
+			keyChainAccessGroup: accessGroupKey
+		)
+		return DependencyContainer(
+			appPreferencesClient: .live(),
+			isAutorizedClient: {
+				guard let authState = tokenStorage.state
+				else { return false }
+				
+				
+				return authState.isAuthorized
 			},
-			oidcAuthValidator: LiveOIDCAuthValidator(
+			oidcAuthStateValidator: LiveOIDCAuthStateValidator(
 				tokenStorage: tokenStorage,
 				clientID: AuthConstant.clientID
 			),
-            authClient: .live(
-                client: .init(
-                    issuer: AuthConstant.issuerURL,
-                    clientID: AuthConstant.clientID,
-                    redirectURI: AuthConstant.redirectURI,
-                    endSessionURI: AuthConstant.endSessionURI
-                ),
-                context: self,
-                tokenStorage: tokenStorage,
-                onInvalidAuth: {
-                    NotificationCenter.default.post(
-                        name: logoutNotification,
-                        object: nil
-                    )
-                }
-            ),
+			authClient: .live(
+				client: .init(
+					issuer: AuthConstant.issuerURL,
+					clientID: AuthConstant.clientID,
+					redirectURI: AuthConstant.redirectURI,
+					endSessionURI: AuthConstant.endSessionURI
+				),
+				context: self,
+				tokenStorage: tokenStorage,
+				onInvalidAuth: {
+					NotificationCenter.default.post(
+						name: logoutNotification,
+						object: nil
+					)
+				}
+			),
 			eventShortClient: EventShortClientImplementation(
 				baseURL: EventsFeatureConstants.clientBaseURL,
 				transport: URLSession.shared
 			),
-            eventShortTypesClient: {
-                if let fileURL = Bundle.main.url(
-                    forResource: "EventShortTypes",
-                    withExtension: "json"
-                ) {
-                    return .live(
-                        baseURL: EventsFeatureConstants.clientBaseURL,
-                        memoryCache: eventsCache,
-                        diskCacheFileURL: fileURL
-                    )
-                } else {
-                    return .live(baseURL: EventsFeatureConstants.clientBaseURL)
-                }
-            }(),
+			eventShortTypesClient: {
+				if let fileURL = Bundle.main.url(
+					forResource: "EventShortTypes",
+					withExtension: "json"
+				) {
+					return .live(
+						baseURL: EventsFeatureConstants.clientBaseURL,
+						memoryCache: eventsCache,
+						diskCacheFileURL: fileURL
+					)
+				} else {
+					return .live(baseURL: EventsFeatureConstants.clientBaseURL)
+				}
+			}(),
 			articleClient: ArticlesClientImplementation(
 				baseURL: NewsFeatureConstants.clientBaseURL,
 				transport: URLSession.shared
-            ), 
-            articleTagsClient: {
-                if let fileURL = Bundle.main.url(
-                    forResource: "ArticleTags",
-                    withExtension: "json"
-                ) {
-                    return ArticleTagsClientImplementation(
-                        baseURL: NewsFeatureConstants.clientBaseURL,
-                        transport: URLSession.shared,
-                        memoryCache: articleTagsCache,
-                        diskCacheFileURL: fileURL
-                    )
-                } else {
-                    return ArticleTagsClientImplementation(
-                        baseURL: NewsFeatureConstants.clientBaseURL,
-                        transport: URLSession.shared,
-                        memoryCache: articleTagsCache
-                    )
-                }
-            }(),
+			), 
+			articleTagsClient: {
+				if let fileURL = Bundle.main.url(
+					forResource: "ArticleTags",
+					withExtension: "json"
+				) {
+					return ArticleTagsClientImplementation(
+						baseURL: NewsFeatureConstants.clientBaseURL,
+						transport: URLSession.shared,
+						memoryCache: articleTagsCache,
+						diskCacheFileURL: fileURL
+					)
+				} else {
+					return ArticleTagsClientImplementation(
+						baseURL: NewsFeatureConstants.clientBaseURL,
+						transport: URLSession.shared,
+						memoryCache: articleTagsCache
+					)
+				}
+			}(),
 			peopleClient: .live(baseURL: MeetFeatureConstants.clientBaseURL),
 			vimeoVideoThumbnailClient: VimeoVideoThumbnailClientImplementation(vimeoOEmbedClient: VimeoOEmbedClientImplementation(transport: URLSession.shared))
 				.cached()
-        )
-    }()
-    
-    var rootCoordinator: RootCoordinator!
-    
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let windowScene = (scene as? UIWindowScene)
-        else { return }
-
-        let window = {
-            let window = BaseWindow(windowScene: windowScene)
-            window.dependencyContainer = dependencyContainer
-            return window
-        }()
-        self.window = window
-        rootCoordinator = RootCoordinator(
-            window: window,
-            dependencyContainer: dependencyContainer
-        )
-        rootCoordinator.start()
-        
-        window.makeKeyAndVisible()
-    }
-    
-    func scene(
-        _ scene: UIScene,
-        openURLContexts URLContexts: Set<UIOpenURLContext>
-    ) {
-        guard let url = URLContexts.first?.url
-        else { return }
-        
-        
-        if let authorizationFlow = self.currentAuthorizationFlow,
-           authorizationFlow.resumeExternalUserAgentFlow(with: url) {
-            currentAuthorizationFlow = nil
-        }
-    }
-    
+		)
+	}()
+	
+	var rootCoordinator: RootCoordinator!
+	
+	func scene(
+		_ scene: UIScene,
+		willConnectTo session: UISceneSession,
+		options connectionOptions: UIScene.ConnectionOptions
+	) {
+		// Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+		// If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+		// This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+		guard let windowScene = (scene as? UIWindowScene)
+		else { return }
+		
+		let window = {
+			let window = BaseWindow(windowScene: windowScene)
+			window.dependencyContainer = dependencyContainer
+			return window
+		}()
+		self.window = window
+		rootCoordinator = RootCoordinator(
+			window: window,
+			dependencyContainer: dependencyContainer
+		)
+		rootCoordinator.start()
+		
+		window.makeKeyAndVisible()
+	}
+	
+	func scene(
+		_ scene: UIScene,
+		openURLContexts URLContexts: Set<UIOpenURLContext>
+	) {
+		guard let url = URLContexts.first?.url
+		else { return }
+		
+		
+		if let authorizationFlow = self.currentAuthorizationFlow,
+		   authorizationFlow.resumeExternalUserAgentFlow(with: url) {
+			currentAuthorizationFlow = nil
+		}
+	}
+	
 }
 
 // MARK: AuthContext
 
 extension SceneDelegate: AuthContext {
-    
-    var presentationContext: () -> UIViewController {
-        { (self.window?.rootViewController)! }
-    }
-    
+	
+	var presentationContext: () -> UIViewController {
+		{ (self.window?.rootViewController)! }
+	}
+	
 }
